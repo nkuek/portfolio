c5006be6da96e361f0f99df422020a559cc54acd
nkuek <nkuek1@gmail.com>
2025-10-23 15:42:37 -0400

    improve hero shader

--- src/components/HeroSection/canvas.tsx
+++ src/components/HeroSection/canvas.tsx
@@ -4,20 +4,19 @@ import { OrbitControls } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { Suspense, useMemo } from "react";
 import {
+  blendColor,
   color,
   float,
   Fn,
   mx_noise_vec3,
-  sin,
   smoothstep,
   uniform,
   uv,
-  vec2,
   vec3,
   vec4,
+  viewportSharedTexture,
 } from "three/tsl";
 import * as THREE from "three/webgpu";
-import { gaussianBlur } from "three/examples/jsm/tsl/display/GaussianBlurNode.js";
 
 function CanvasContent() {
   const { viewport } = useThree();
@@ -25,20 +24,23 @@ function CanvasContent() {
     const time = uniform(0);
     const uniforms = { time };
     const colorNode = Fn(() => {
-      const baseColor = color("#D98CA2").toVar();
+      const baseColor = color("#ba6077").toVar();
       const noise = mx_noise_vec3(
-        vec3(uv().x.mul(3), uv().y.mul(2), time.mul(0.3)),
+        vec3(
+          uv().x.mul(1 * viewport.width * 0.15),
+          uv().y.mul(1 * viewport.height * 0.15),
+          time.mul(0.1),
+        ),
       );
-      baseColor.addAssign(noise.y.mul(0.4));
-      // const c = baseColor
-      //   .mul(0.5 + 0.5 * sin(noise.x.add(time)))
-      //   .add(vec3(0.1, 0.2, 0.3).mul(sin(time * 2.0 + noise.y * 10.0)));
-      // const blur = gaussianBlur(vec4(uv().mul(time), 0.0, 1.0), 0.01, 30);
-      return vec4(baseColor, float(0.7).sub(smoothstep(0.1, 0.5, noise.y)));
+      baseColor.addAssign(noise.x.mul(0.4));
+      return blendColor(
+        vec4(baseColor, float(0.5).sub(smoothstep(0.1, 0.3, noise.x))),
+        viewportSharedTexture(),
+      );
     })();
 
     return { colorNode, uniforms };
-  }, []);
+  }, [viewport.width, viewport.height]);
 
   useFrame((_, delta) => {
     uniforms.time.value += delta;
@@ -48,7 +50,6 @@ function CanvasContent() {
       <mesh scale={[viewport.width, viewport.height, 1]}>
         <planeGeometry args={[1, 1]} />
         <meshBasicNodeMaterial
-          // colorNode={colorNode}
           fragmentNode={colorNode}
           key={THREE.MathUtils.generateUUID()}
           transparent
@@ -61,7 +62,7 @@ function CanvasContent() {
 
 export default function HeroSectionCanvas() {
   return (
-    <div className="animate-fade-in absolute inset-0 z-[-1] h-dvh w-full opacity-0">
+    <div className="animate-fade-in absolute inset-0 -top-19 z-[-1] h-dvh w-full opacity-0">
       <WebGPUCanvas>
         <Suspense fallback={<div>Loading...</div>}>
           <CanvasContent />
