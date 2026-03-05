0d0a779f24d3d35552090e1c0a783c578af6af8a
nkuek <nkuek1@gmail.com>
2026-03-05 13:22:40 -0500

    remove unused DSButton, DSButtonLink, and DSLinkContainer components

--- src/design-system/DSButton/types.ts
+++
@@ -1,6 +0,0 @@
-import { VariantProps } from "tailwind-variants";
-import { buttonVariants } from "./variants";
-
-export type DSButtonVariantProps = VariantProps<typeof buttonVariants>;
-export type DSButtonProps = DSButtonVariantProps &
-  React.ButtonHTMLAttributes<HTMLButtonElement>;
